a59bd172af84bb88a9a6bbfbc83fcc94f15f8882
Admin <admin@Admins-MacBook-Pro.local>
2024-07-26 13:38:05 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <ul>
+      <li>Hello!! I am Harshini I, a final-year student currently pursuing B.Tech in Information technology in Saveetha Engineering College, Thandalam.</li>
+      <li> I have a solid academic foundation, having achieved 91% in HSC and 93.2% in SSLC from DAV School, Adambakkam</li>
+      <li>I have scored 90% in CBSE Board Examination and currently with a CGPA of 8 as of semester-6.</li>
+      <li>I have a basic programming knowledge in Python, C, My-SQL & C++.</li>
+      <li>Did an one month internship in Edify Techno Solutions on Machine learning. </li>
+      <li>Languages known are English, Tamil, Hindi and German.</li>
+      <li>My hobbies are producing music and performing live as an keyboardist.</li>
+    </ul>
+  </section>
+);
+
+export default About;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => (
+  <section id="projects">
+    <h2>Projects</h2>
+    <ul>
+      <h3>Smart Traffic Signal Management using IoT</h3>
+<li>Designed and implemented an IoT-based solution for dynamic traffic signal control</li>
+<li>Utilized sensors to monitor traffic density and adjust signal timings accordingly
+.</li>
+    </ul>
+  </section>
+);
+export default Projects;
